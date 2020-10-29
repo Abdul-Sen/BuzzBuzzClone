0c487077fbc54224ee86d6157c54d087f78c1c9a
Abdul Rehman <abdul-rehman@myseneca.ca>
2020-10-29 01:06:18 +0000

    project controller and view

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Showcase.Data;
 using Showcase.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Showcase.Controllers
 {
@@ -17,11 +19,42 @@ namespace Showcase.Controllers
         {
             _controllerContext = appDb;
         }
-
         public IActionResult Index()
         {
             Console.WriteLine("ONE FROM PROJECT");
             return View(_controllerContext.Project.ToList());
+        }
+
+
+        [HttpGet]
+        [Route("/Project/ProjectDetails/{projectId}")]
+        public IActionResult ProjectDetails(int projectId)
+        {
+            // C ProjectDetails pd = new uProjectDetails();
+            Console.WriteLine(projectId);
+            List<ProjectModel> allInfo = new List<ProjectModel>(
+                            _controllerContext.Project.
+                            Where(x => x.Id == projectId).
+                            Include(project => project.Builder).
+                            Include(project => project.Address).
+                            ToList());
+
+                        
+            List<UnitsModel> unitsForProject = new List<UnitsModel>(_controllerContext.Units.Where(x => x.projectId == projectId));
+            allInfo.ForEach(Console.WriteLine);
+            
+            // will pass info to view
+            return View("Detail", new Showcase.Models.Custom.ProjectDetails { UnitsList = unitsForProject, ProjectRecord = allInfo[0] });
+        }
+
+        [HttpGet]
+        [Route("/Project/UnitDetails/{unitId}")]
+        public IActionResult UnitDetails(int unitId)
+        {
+            Console.WriteLine("unit detail action called");            
+            Console.WriteLine($"UNIT ID IS.. {unitId}");
+
+            return Redirect("~/Home/Index");
         }
     }
 }
